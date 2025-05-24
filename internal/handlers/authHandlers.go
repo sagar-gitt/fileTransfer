886211b2374a4a2ccb23d4bd6e117f654b659553
Sinha-Sagar <sinhasagar89@gmail.com>
2025-05-24 16:58:50 +0530

    backend

---
+++ internal/handlers/authHandlers.go
@@ -0,0 +1,80 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fileTransfer/internal/utils"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"io"
+	"net/http"
+	"os"
+)
+
+var (
+	googleOauthConfig *oauth2.Config
+)
+
+func InitGoogleAuth() {
+	googleOauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func (h *Handlers) GoogleLogin(c *gin.Context) {
+	url := googleOauthConfig.AuthCodeURL("random")
+	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func (h *Handlers) GoogleCallback(c *gin.Context) {
+	code := c.Query("code")
+	token, err := googleOauthConfig.Exchange(c, code)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Code exchange failed"})
+		return
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user info"})
+		return
+	}
+	defer response.Body.Close()
+
+	userData, err := io.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read user info"})
+		return
+	}
+
+	var userInfo struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	}
+	if err := json.Unmarshal(userData, &userInfo); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse user info"})
+		return
+	}
+
+	// Generate JWT token
+	jwtToken, err := h.JwtService.GenerateToken(userInfo.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	// Redirect to frontend with token
+	frontendURL := os.Getenv("FRONTEND_URL")
+	if frontendURL == "" {
+		frontendURL = "http://localhost:5173"
+	}
+	c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("%s/auth/callback?token=%s", frontendURL, jwtToken))
+} 
